886c3e1b34b29b941dbdefc9a50eecb127f0f473
Unknown <luke.logan@northstarmarketing.com>
2018-08-17 11:47:37 -0400

    Example of PROMPT script

--- js-course/scripts.js
+++ js-course/scripts.js
@@ -1,6 +1,5 @@
 // document represents the current page that you are on. You can use JS to write info onto
 //the current page you are on. 
-var Message = "Hello";
-alert(Message); //notice how there are no quotes around "message" to call the variable. 
-Message = "welcome to Luke's awesome treehouse course!"
-alert(Message);
+var visitor_name = prompt("what is your name?");
+console.log(visitor_name);
+alert(visitor_name);
