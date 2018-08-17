932a8723c000687e67cb5654240af41e80732ae9
Unknown <luke.logan@northstarmarketing.com>
2018-08-17 10:53:00 -0400

    Code through video "The JS console"

---
+++ js-course/scripts.js
@@ -0,0 +1,4 @@
+// document represents the current page that you are on. You can use JS to write info onto
+//the current page you are on. 
+alert("This is my first JS code!");
+document.write("<h1>Welcome to the Javascript course!</h1>");
