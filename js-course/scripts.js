6d5e21362dcd4eaef024846edcbcac2109bed858
Unknown <luke.logan@northstarmarketing.com>
2018-08-17 11:11:23 -0400

    Creates a variable

--- js-course/scripts.js
+++ js-course/scripts.js
@@ -1,2 +1,4 @@
 // document represents the current page that you are on. You can use JS to write info onto
 //the current page you are on. 
+var Message = "Hello";
+alert(Message); //notice how there are no quotes around "message" to call the variable. 
