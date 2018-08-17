03f64146d373ed7e9d72d25351c3eb3386ed3d32
Unknown <luke.logan@northstarmarketing.com>
2018-08-17 13:42:48 -0400

    Practice with methods

--- js-course/scripts.js
+++ js-course/scripts.js
@@ -1,5 +1,10 @@
 // document represents the current page that you are on. You can use JS to write info onto
 //the current page you are on. 
-var visitor = prompt('What is your name?');
-var message = 'Hello ' + visitor;
-document.write('<h4>'+ message + '</h4>');
+var passphrase = "Open Sesame";
+console.log(passphrase.length);
+console.log(passphrase.toLowerCase());//to lower case returns a value.
+
+//shout app
+var string_to_shout =  prompt("What would you like to shout?");
+var string_to_shout = string_to_shout.toUpperCase();
+document.write('<h6>'+ string_to_shout + '!!!!</h6>');
