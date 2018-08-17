fbc1ae7859f8d5fa518a4303566f6dbfec0e1ac6
Unknown <luke.logan@northstarmarketing.com>
2018-08-17 14:37:01 -0400

    How many minutes alive? function

--- js-course/scripts.js
+++ js-course/scripts.js
@@ -1,10 +1,16 @@
 // document represents the current page that you are on. You can use JS to write info onto
 //the current page you are on. 
-var passphrase = "Open Sesame";
-console.log(passphrase.length);
-console.log(passphrase.toLowerCase());//to lower case returns a value.
+var secondsPerMin = 60;
+var minutesPerHr = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
 
-//shout app
-var string_to_shout =  prompt("What would you like to shout?");
-var string_to_shout = string_to_shout.toUpperCase();
-document.write('<h6>'+ string_to_shout + '!!!!</h6>');
+var secondsPerDay = secondsPerMin * minutesPerHr * hoursPerDay;
+document.write('There are ' + secondsPerDay + ' seconds in a day.')
+
+var yearsAlive = prompt("how old are you?");
+
+var minutesPerYear = minutesPerHr * hoursPerDay * daysPerWeek * weeksPerYear;
+var minutesAlive = yearsAlive * minutesPerYear;
+document.write('<p>You have been alive for ' + minutesAlive + ' minutes. WOW that is long!</p>')
