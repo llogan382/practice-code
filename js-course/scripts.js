aa4fc97ce781d35d0bffa00d665c50b20590432d
Unknown <luke.logan@northstarmarketing.com>
2018-08-17 20:35:06 -0400

    Practice if/else statements

--- js-course/scripts.js
+++ js-course/scripts.js
@@ -1,3 +1,6 @@
-var roll = Math.floor( Math.random() * 6);
-
-document.write('Your dice roll is ' + roll);
+var answer = prompt("What programming language is also the name of a gem?");
+if( answer.toUpperCase() === 'RUBY'){ 
+    document.write("<p> That's right!</p>");
+} else {
+    document.write("<p>Sorry, that is incorrect</p>")
+}
