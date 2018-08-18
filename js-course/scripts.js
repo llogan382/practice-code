f23ee16452c6f1d08a508f60497bf0e1e2599b33
Unknown <luke.logan@northstarmarketing.com>
2018-08-18 10:27:46 -0400

    Adds boolean evaluator to dice roll

--- js-course/scripts.js
+++ js-course/scripts.js
@@ -1,3 +1,4 @@
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6 ) +1;
 var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
 if (parseInt(guess)=== randomNumber) {
