8cdad63c0e2461f41463ed4d371ac8fffc872688
Unknown <luke.logan@northstarmarketing.com>
2018-08-18 10:47:06 -0400

    adds iteration for dice roll.

--- js-course/scripts.js
+++ js-course/scripts.js
@@ -2,7 +2,20 @@ var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6 ) +1;
 var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
 if (parseInt(guess)=== randomNumber) {
-    document.write('<p>You guessed the right number!</p>');
+    correctGuess = true;
+} else if ( parseInt(guess) < randomNumber) {
+    var guessMore = prompt("try again. The number I am thinking of is more than " + guess);
+    if (parseInt(guessMore) === randomNumber ) {
+        correctGuess = true;
+    }
+} else if ( parseInt(guess) > randomNumber ){
+    var guessLess = prompt("Try again. The number I am thinking of is less than " + guess);
+    if ( parseInt(guessLess) === randomNumber) {
+        correctGuess = true;
+    }
+}
+if ( correctGuess ) {
+    document.write('<p>You guessed the number!</p>');
 } else {
-    document.write('<p>sorry, you guessed wrong. The correct numbr is ' + randomNumber + '</p>');
+    document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
 }
