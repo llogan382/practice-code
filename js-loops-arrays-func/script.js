bfc7a865043e66ccd87dcac2311b8b0971cbffe7
Unknown <luke.logan@northstarmarketing.com>
2018-08-21 14:20:38 -0400

    Creates 10 random colors

--- js-loops-arrays-func/script.js
+++ js-loops-arrays-func/script.js
@@ -1,6 +1,16 @@
-var html = "";
+var html = '';
+var red;
+var green;
+var blue;
+var rgbColor;
 
-for (let i = 0; i <= 100; i++) {
-  html += '<p>' + i + ' guesses right </p>';
+
+for( i=0; i<10; i++ ) {
+red = Math.floor(Math.random() * 256 );
+green = Math.floor(Math.random() * 256 );
+blue = Math.floor(Math.random() * 256 );
+rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+html += '<div style="background-color:' + rgbColor + '"></div>';
 }
+
 document.write(html);
