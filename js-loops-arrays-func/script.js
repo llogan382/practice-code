2d50b7b681acd07ed026acf3f73da6fa86d3bfd9
Unknown <luke.logan@northstarmarketing.com>
2018-08-21 13:26:02 -0400

    Human guess random number

--- js-loops-arrays-func/script.js
+++ js-loops-arrays-func/script.js
@@ -1,16 +1,23 @@
-var upper = 10000;
-var randomnumber = getrandomNumber(upper);
+var randomnumber = getrandomNumber(10);
 var guess;
-var attempts = 0.
+var guessCount = 0.
+var correctGuess = false;
+
+
 
 function getrandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
 
 
-while ( guess !== randomnumber ) {
-  guess = getrandomNumber(upper );
-  attempts += 1;
-}
+do {
+  guess = prompt("guess a number between 1 amd 10")
+  guessCount += 1;
+ if (parseInt(guess) === randomnumber ) {
+    correctGuess = true;
+  }
+} while ( ! correctGuess );
+
+
 document.write('the correct number was ' + randomnumber + '.')
-document.write(' It took the computer ' + attempts + ' number of times to guess right. ');
+document.write('<p> It took the you ' + guessCount + ' number of times to guess right.</p> ');
