d0c048f03093b35740f03403f009d1a4963abce0
Unknown <luke.logan@northstarmarketing.com>
2018-08-21 13:09:06 -0400

    computer guessing random computer numbers

--- js-loops-arrays-func/script.js
+++ js-loops-arrays-func/script.js
@@ -1,3 +1,16 @@
-function randomNumber(upper) {
+var upper = 10000;
+var randomnumber = getrandomNumber(upper);
+var guess;
+var attempts = 0.
+
+function getrandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+
+
+while ( guess !== randomnumber ) {
+  guess = getrandomNumber(upper );
+  attempts += 1;
+}
+document.write('the correct number was ' + randomnumber + '.')
+document.write(' It took the computer ' + attempts + ' number of times to guess right. ');
